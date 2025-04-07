1760645463cb0a02a28a85c101ad3525daef1d62
leandro-costa <leandro.costa@ifba.edu.br>
2025-04-07 10:51:30 -0300

    first commit

---
+++ src/.vuepress/navbar.ts
@@ -0,0 +1,40 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/",
+  {
+    text: "Publicações",
+    icon: "pen-to-square",
+    link: "/article/",
+  },
+  {
+    text: "Categoria",
+    icon: "list",
+    link: "/category/",
+  },
+  {
+    text: "Tag",
+    icon: "tag",
+    link: "/tag/",
+  },
+  {
+    text: "Exercícios",
+    icon: "book",
+    link: "/category/exercicio/",
+  },
+  {
+    text: "Trabalho",
+    icon: "laptop-code",
+    link: "/category/trabalho/",
+  },
+  {
+    text: "Para Entrega",
+    icon: "paper-plane",
+    link: "/category/entrega/",
+  },
+  {
+    text: "Timeline",
+    icon: "clock",
+    link: "/timeline/",
+  },
+]);
